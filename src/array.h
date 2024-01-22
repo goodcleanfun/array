70ed564cf4e316bb726e4dccf8ed612542718dbd
Al <albarrentine@gmail.com>
2024-01-21 23:32:11 -0500

    [align] moving aligned memory to a separate repo

--- src/array.h
+++ src/array.h
@@ -6,57 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
-#if defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)
-#include <malloc.h>
-static inline void *aligned_malloc(size_t size, size_t alignment) {
-    return _aligned_malloc(size, alignment);
-}
-static inline void *aligned_resize(void *p, size_t old_size, size_t new_size, size_t alignment) {
-    return _aligned_realloc(p, new_size, alignment);
-}
-static inline void aligned_free(void *p) {
-    _aligned_free(p);
-}
-#else
-#include <stdlib.h>
-static inline void *aligned_malloc(size_t size, size_t alignment)
-{
-    void *p;
-    int ret = posix_memalign(&p, alignment, size);
-    return (ret == 0) ? p : NULL;
-}
-static inline void *aligned_resize(void *p, size_t old_size, size_t new_size, size_t alignment)
-{
-    if ((alignment == 0) || ((alignment & (alignment - 1)) != 0) || (alignment < sizeof(void *))) {
-        return NULL;
-    }
-
-    if (p == NULL) {
-        return NULL;
-    }
-
-    void *p1 = aligned_malloc(new_size, alignment);
-    if (p1 == NULL) {
-        free(p);
-        return NULL;
-    }
-
-    memcpy(p1, p, old_size);
-    free(p);
-    return p1;
-}
-static inline void aligned_free(void *p)
-{
-    free(p);
-}
-#endif
-
-#ifdef _MSC_VER
-#define MIE_ALIGN(x) __declspec(align(x))
-#else
-#define MIE_ALIGN(x) __attribute__((aligned(x)))
-#endif
+#include "aligned/aligned.h"
 
 #endif // ARRAY_H
 
